c1dc6b7824f0dee4c96f561a2b93cfb3caf8b528
NKRISTIN <105353598+NKRISTIN@users.noreply.github.com>
2022-05-12 19:16:43 +0800

    WTM BTC tour
    2nd time making a web tour

---
+++ data.js
@@ -0,0 +1,164 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stone-plaque-of-nus-law",
+      "name": "Stone Plaque of NUS Law",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.119164285664791,
+        "pitch": -1.5707963267948966,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0527636243481515,
+          "pitch": -0.13467628279923893,
+          "rotation": 1.5707963267948966,
+          "target": "1-c-j-koh-law-library-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6381539817702659,
+          "pitch": -0.0067975573294276614,
+          "title": "Welcome to.....The Bukit Timah Campus of NUS!&nbsp;",
+          "text": "This here is the Singapore Botanic Gardens seed bank! Isn't it amazing that we are storing seeds of tomorrow?"
+        },
+        {
+          "yaw": -2.438678914734929,
+          "pitch": -0.22577476512238448,
+          "title": "Ahh the hard to miss: Faculty of LAW Stone Plaque plague&nbsp;",
+          "text": "Err its basically exactly what it says in the description. But don't worry, I'm sure its going to look great with you and your friends in the background!"
+        }
+      ]
+    },
+    {
+      "id": "1-c-j-koh-law-library-",
+      "name": "C J Koh Law Library ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.127396494698586,
+        "pitch": -0.5112702035455889,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1162208834467897,
+          "pitch": -0.15722738592714158,
+          "rotation": 1.5707963267948966,
+          "target": "2-the-summit-canteen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3845121255572721,
+          "pitch": 0.05910996297438942,
+          "title": "Next up is~ THE 1 and ONLY : *CJ Koh LAW LIB*",
+          "text": "<div>For all those bookies out there, thiss here is the bomb !! Make sure to come down at least once to pick out the gems that this library has to offer.</div><div><br></div>"
+        },
+        {
+          "yaw": -1.9960084688931587,
+          "pitch": 0.19337724885532737,
+          "title": "One wing&nbsp;",
+          "text": "not too sure which is which oops"
+        },
+        {
+          "yaw": 1.4791337791591896,
+          "pitch": 0.34850602218789106,
+          "title": "The Other wing",
+          "text": "Tbh, it probably ain't gonna matter which wing you use hahah!"
+        }
+      ]
+    },
+    {
+      "id": "2-the-summit-canteen",
+      "name": "The Summit Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.4306431827796207,
+        "pitch": 0,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4864808239238876,
+          "pitch": -0.1912145572215298,
+          "rotation": 4.71238898038469,
+          "target": "0-stone-plaque-of-nus-law"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4243064384798902,
+          "pitch": 0,
+          "title": "And finally, this here is the Summit Canteen~",
+          "text": "I think its quiet aptly named by reaching the summit of out highest hill in Singapore: Bukit Timah Hill. But who knows exactly why, maybe its was just a steep incline to success?"
+        }
+      ]
+    }
+  ],
+  "name": "NUS Bukit Timah CampUS tour ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
